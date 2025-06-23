eee42e8fa9330cc7c458f665796f04ca1ff1b6d6
Piotrek Budzowski <piobudzows@gmail.com>
2025-06-23 17:59:18 +0200

    Create ejercicio1.js

---
+++ Tema_1/ejercicio1.js
@@ -0,0 +1,16 @@
+// DECLARANDO VARIABLES
+const nombre = "Cristobal";
+let edad = 34;
+var esEstudiante = true;
+
+// VARIABLE NOMBRE
+console.log("Nombre:", nombre);
+console.log("Tipo:", typeof nombre);
+
+// VARIABLE EDAD
+console.log("Edad:", edad);
+console.log("Tipo:", typeof edad);
+
+// VARIABLE ES ESTUDAINTE
+console.log("Es estudiante: ", esEstudiante ? "Si" : "No");
+console.log("Tipo:", typeof esEstudiante);
