b3ea8ef62202fc0cdb42f6f991bd7712eba0318f
Piotrek Budzowski <piobudzows@gmail.com>
2025-06-23 17:59:22 +0200

    Create ejercicio3.js

---
+++ Tema_1/ejercicio3.js
@@ -0,0 +1,11 @@
+// DECLARANDO VARIABLES
+let temperatura = 20
+
+// EL BLOQUE DE IF CON SU ELSE IF Y ELSE
+if (temperatura > 25) {
+    console.log("Hace calor");
+} else if (temperatura >= 15 && temperatura <= 25) {
+    console.log("Temperatura agradable");
+} else {
+    console.log("Hace frio");
+}
