526e44e7edce7cc127edc0d42eae9ee286dc991f
Piotrek Budzowski <piobudzows@gmail.com>
2025-06-23 17:59:19 +0200

    Create ejercicio2.js

---
+++ Tema_1/ejercicio2.js
@@ -0,0 +1,10 @@
+// DECLARANDO VARIABLES
+let num1 = 1
+let num2 = 2
+
+// IMPRIMIENDO RESULTADOS DE OPERACIONES
+console.log("Suma: ", num1 + num2)
+console.log("Multiplicacion: ", num1 * num2)
+console.log("Resta: ", num1 % num2)
+console.log("Es estrictamente igual: ", (num1 === num2) ? "Verdad" : "Mentira")
+console.log("Son diferente: ", (num1 != num2) ? "Verdad" : "Mentira")
